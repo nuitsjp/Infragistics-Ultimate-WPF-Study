0f4046cc7bc78693bc81c8df93b1c561f4fd9f9f
Atsushi Nakamura <nuits.jp@live.jp>
2019-06-11 05:34:56 +0900

    DragAndDropWithXamTreeGridの追加

---
+++ DragAndDropWithXamTreeGrid/DragAndDropWithXamTreeGrid/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Lightningfall;
+
+namespace DragAndDropWithXamTreeGrid
+{
+    public class MainWindowViewModel
+    {
+        private readonly List<Task> _tasks = new List<Task>();
+
+        public MainWindowViewModel()
+        {
+            AddTask(new Task("業務分析"));
+            AddTask(new Task("ドメイン分析"));
+            AddTask(new Task("アーキテクチャ設計"));
+            AddTask(new Task("プロダクト バックログ"));
+            AddTask(new Task("スプリント ダッシュボード"));
+            AddTask(new Task("プロジェクト スケジュール"));
+            AddTask(new Task("プロジェクト カレンダー"));
+            AddTask(new Task("プロジェクト リソース"));
+
+            var task = Tasks.First();
+            task.AddTask(new Task("ブレスト"));
+            task.AddTask(new Task("業務モデル作成"));
+            task.Tasks[1].AddTask(new Task("作成"));
+            task.Tasks[1].AddTask(new Task("社内レビュー"));
+            task.Tasks[1].AddTask(new Task("社内レビューバック"));
+            task.Tasks[1].AddTask(new Task("社外レビュー"));
+            task.Tasks[1].AddTask(new Task("社外レビューバック"));
+
+        }
+
+        public IReadOnlyList<Task> Tasks => _tasks;
+        public void AddTask(Task task)
+        {
+            _tasks.Add(task);
+        }
+    }
+}
---
+++ DragAndDropWithXamTreeGrid/DragAndDropWithXamTreeGrid/Task.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lightningfall
+{
+    public class Task
+    {
+        private readonly List<Task> _tasks = new List<Task>();
+
+        private static int _maxTaskId = 0;
+
+        public Task(string name)
+        {
+            Name = name;
+        }
+
+
+        public int TaskId { get; } = _maxTaskId++;
+        public string Name { get; }
+
+        public IReadOnlyList<Task> Tasks => _tasks;
+
+        public void AddTask(Task task)
+        {
+            _tasks.Add(task);
+        }
+    }
+}
