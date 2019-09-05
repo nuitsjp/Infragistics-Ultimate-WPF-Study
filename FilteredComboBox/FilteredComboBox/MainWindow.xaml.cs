8fd72e4f1c4f3ac4a546bbdcf8d75f6fdbc9b4e1
Atsushi Nakamura <nuits.jp@live.jp>
2019-09-05 15:05:31 +0900

    Add FilteredComboBox

---
+++ FilteredComboBox/FilteredComboBox/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CsvHelper;
+
+namespace FilteredComboBox
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Product> Products { get; }
+        public MainWindow()
+        {
+            using (var csv = new CsvReader(new StreamReader("Products.csv")))
+            {
+                var config = csv.Configuration;
+                config.HasHeaderRecord = true; // ヘッダーが存在する場合 true
+                Products = csv.GetRecords<Product>().ToList();
+            }
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
