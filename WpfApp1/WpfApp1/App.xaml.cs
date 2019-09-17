14ccf5b91e74281978861b74eee927c18908c28e
Atsushi Nakamura <nuits.jp@live.jp>
2019-09-18 07:25:34 +0900

    サンプル追加

---
+++ WpfApp1/WpfApp1/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Infragistics.Themes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            ThemeManager.ApplicationTheme = new RoyalLightTheme();
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MaterialWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MaterialWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MaterialWindow : Window
+    {
+        public MaterialWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UIElement_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //until we had a StaysOpen glag to Drawer, this will help with scroll bars
+            var dependencyObject = Mouse.Captured as DependencyObject;
+            while (dependencyObject != null)
+            {
+                if (dependencyObject is ScrollBar) return;
+                dependencyObject = VisualTreeHelper.GetParent(dependencyObject);
+            }
+
+            MenuToggleButton.IsChecked = false;
+        }
+
+        private async void MenuPopupButton_OnClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void OnCopy(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (e.Parameter is string stringValue)
+            {
+                try
+                {
+                    Clipboard.SetDataObject(stringValue);
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine(ex.ToString());
+                }
+            }
+        }
+
+    }
+}
