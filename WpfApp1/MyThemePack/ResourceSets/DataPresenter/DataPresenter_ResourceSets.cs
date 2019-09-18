0479ab04a657c898c513ce357a87e35d52a022b1
Atsushi Nakamura <nuits.jp@live.jp>
2019-09-19 07:18:23 +0900

    テーマ追加

---
+++ WpfApp1/MyThemePack/ResourceSets/DataPresenter/DataPresenter_ResourceSets.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using Infragistics.Windows;
+using Infragistics.Windows.Themes;
+
+// ReSharper disable once CheckNamespace
+namespace MyThemePack.ResourceSets.OrangePeel
+{
+    public class DataPresenter : DataPresenterResourceSet<Locator>
+    {
+        private static DataPresenter _instance;
+        public static DataPresenter Instance => _instance ?? (_instance = new DataPresenter());
+    }
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class Locator : ResourceSetLocator
+    {
+        public override Assembly Assembly => this.GetType().Assembly;
+        public override string Theme => "OrangePeel";
+        public override string Grouping => DataPresenterGeneric.Instance.Grouping;
+        public override string ResourcePath => @"ResourceSets\DataPresenter\DataPresenter.xaml";
+    }
+}
---
+++ WpfApp1/MyThemePack/ThemesOrangePeelAssemblyResourceSetLoader.cs
@@ -0,0 +1,33 @@
+﻿using Infragistics.Windows.Themes;
+
+// ReSharper disable once CheckNamespace
+namespace MyThemePack.ResourceSets.OrangePeel
+{
+    public class ThemesOrangePeelAssemblyResourceSetLoader : AssemblyResourceSetLoader
+    {
+        protected override void OnRegisterResourceSets(string groupingName)
+        {
+            if (groupingName == null)
+            {
+                //ThemesOrangePeelAssemblyResourceSetLoader.RegisterResourceSet(OrangePeel.Primitives.Instance);
+                //ThemesOrangePeelAssemblyResourceSetLoader.RegisterResourceSet(OrangePeel.Editors.Instance);
+                ThemesOrangePeelAssemblyResourceSetLoader.RegisterResourceSet(OrangePeel.DataPresenter.Instance);
+            }
+            else
+            {
+                switch (groupingName)
+                {
+                    case "Primitives":
+                        //ThemesOrangePeelAssemblyResourceSetLoader.RegisterResourceSet(OrangePeel.Primitives.Instance);
+                        break;
+                    case "DataPresenter":
+                        ThemesOrangePeelAssemblyResourceSetLoader.RegisterResourceSet(OrangePeel.DataPresenter.Instance);
+                        break;
+                    case "Editors":
+                        //ThemesOrangePeelAssemblyResourceSetLoader.RegisterResourceSet(OrangePeel.Editors.Instance);
+                        break;
+                }
+            }
+        }
+    }
+}
